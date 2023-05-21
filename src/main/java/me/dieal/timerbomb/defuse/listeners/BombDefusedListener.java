d1941cbc45887fd8cdcbe230b5edeb1a87cc9d93
Dieal <diealblack2005@gmail.com>
2023-05-21 17:27:03 +0200

    Added defuse report

--- src/main/java/me/dieal/timerbomb/defuse/listeners/BombDefusedListener.java
+++ src/main/java/me/dieal/timerbomb/defuse/listeners/BombDefusedListener.java
@@ -3,6 +3,8 @@ package me.dieal.timerbomb.defuse.listeners;
 import me.dieal.timerbomb.bomb.listeners.Bomb;
 import me.dieal.timerbomb.bomb.listeners.BombsManager;
 import me.dieal.timerbomb.defuse.inventories.BombDefusedInventory;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +12,9 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+
+import java.util.ArrayList;
 
 public class BombDefusedListener implements Listener {
 
@@ -34,8 +39,8 @@ public class BombDefusedListener implements Listener {
 
         switch (clickedItem.getType()) {
             case BOOK:
-                // gives the report book to the player
                 player.closeInventory();
+                player.getInventory().addItem(createReport(bomb));
                 break;
             case BARRIER:
                 player.closeInventory();
@@ -44,6 +49,36 @@ public class BombDefusedListener implements Listener {
                 break;
         }
 
+    }
+
+    private ItemStack createReport (Bomb bomb) {
+
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta meta = (BookMeta) book.getItemMeta();
+
+        ArrayList<String> pages = new ArrayList<>();
+        pages.add(createPage(bomb));
+        meta.setTitle("Defuse " + bomb.getDefuseTime());
+        meta.setPages(pages);
+        meta.setAuthor("System");
+        book.setItemMeta(meta);
+
+        return book;
+
+    }
+
+    private String createPage(Bomb bomb) {
+        StringBuilder builder = new StringBuilder();
+        String title = ChatColor.DARK_RED + "" + ChatColor.BOLD + "Report" + ChatColor.BLACK;
+        builder.append("       [ " + title + " ]");
+
+        builder.append("\n\n " + ChatColor.RED + "Defuser:\n" + ChatColor.BLACK);
+        builder.append(" " + bomb.getDefuserName());
+
+        builder.append("\n\n " + ChatColor.RED + "Date and time:\n" + ChatColor.BLACK);
+        builder.append(" " + bomb.getDefuseTime());
+
+        return builder.toString();
     }
 
 }
